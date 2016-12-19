9c717b1015e45873cd5367ec9ca9d1141d4658ab
Pawel Misiurski <pawel.misiurski@bookatable.com>
2016-12-19 12:02:13 +0000

    back to original makeArray shim

--- jQueryShim.js
+++ jQueryShim.js
@@ -83,7 +83,7 @@ module.exports = jQueryDeferred.extend(
   inArray: (arr, item) => arr.indexOf(item) !== -1,
   trim: str => str && str.trim(),
   isEmptyObject: obj => !obj || Object.keys(obj).length === 0,
-  makeArray: arr => Array.of(arr),
+  makeArray: arr => [].slice.call(arr, 0),
   support: {
     cors: false
   }
