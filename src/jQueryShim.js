402cd340119a3bca8535beb11ffc3e1f989750d5
Erik Engstrand <erik@bokio.se>
2020-04-03 08:34:38 +0200

    Add headers to jquery shim

--- src/jQueryShim.js
+++ src/jQueryShim.js
@@ -81,8 +81,22 @@ const ajax = function(options) {
   };
 
   request.withCredentials = options.xhrFields.withCredentials;
+  var cacheBuster = "&_=" + new Date().getTime();
+  if (options.url.indexOf("&_=") === -1) {
+    options.url += cacheBuster;
+  }
+  else {
+    options.url.replace(/&_=\d+/, cacheBuster);
+  }
   request.open(options.type, options.url);
   request.setRequestHeader('content-type', options.contentType);
+  if (options.headers) {
+    Object.keys(options.headers).forEach(key => {
+      const value = options.headers[key];
+      request.setRequestHeader(key, value);
+    });
+  }
+  
   request.send(options.data.data && `data=${encodeURIComponent(options.data.data)}`);
 
   return {
