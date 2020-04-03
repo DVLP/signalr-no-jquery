07dd55be6e67adf57b2231da5ff5043d2f066cb1
Erik Engstrand <erik@bokio.se>
2020-04-03 08:34:38 +0200

    Add headers to jquery shim

--- build/jQueryShim.js
+++ build/jQueryShim.js
@@ -87,14 +87,21 @@ var ajax = function ajax(options) {
   };
 
   request.withCredentials = options.xhrFields.withCredentials;
+  var cacheBuster = "&_=" + new Date().getTime();
+  if (options.url.indexOf("&_=") === -1) {
+    options.url += cacheBuster;
+  } else {
+    options.url.replace(/&_=\d+/, cacheBuster);
+  }
   request.open(options.type, options.url);
   request.setRequestHeader('content-type', options.contentType);
   if (options.headers) {
-    Object.keys(options.headers).forEach(key => {
-      const value = options.headers[key];
+    Object.keys(options.headers).forEach(function (key) {
+      var value = options.headers[key];
       request.setRequestHeader(key, value);
     });
   }
+
   request.send(options.data.data && 'data=' + encodeURIComponent(options.data.data));
 
   return {
